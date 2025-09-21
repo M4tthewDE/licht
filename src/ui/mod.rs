e5547a12e48771416d292a7d470b5569562e1111
M4tthewDE <matthias.kronberg@gmail.com>
2025-09-21 19:30:32 +0200

    feat: automatically search

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -5,6 +5,7 @@ use egui::{
 use std::{
     collections::BTreeMap,
     sync::mpsc::{Receiver, Sender},
+    time::Instant,
 };
 use tokio::runtime::{Builder, Runtime};
 
@@ -12,7 +13,7 @@ use egui::{Color32, FontId};
 
 use crate::{
     Config,
-    tmdb::{MovieSearchResponse, TmdbClient},
+    tmdb::{MovieSearchResponse, MovieSearchResult, TmdbClient},
 };
 
 pub struct LichtApp {
@@ -22,6 +23,7 @@ pub struct LichtApp {
     rx: Receiver<MovieSearchResponse>,
     tx: Sender<MovieSearchResponse>,
     movie_search_response: Option<MovieSearchResponse>,
+    last_change_time: Instant,
 }
 
 impl eframe::App for LichtApp {
@@ -42,6 +44,20 @@ impl eframe::App for LichtApp {
             style.visuals.override_text_color = Some(Color32::WHITE);
         });
 
+        if Instant::now()
+            .duration_since(self.last_change_time)
+            .as_millis()
+            >= 250
+        {
+            let tmdb_client = self.tmdb_client.clone();
+            let search_text = self.search_text.clone();
+            let tx = self.tx.clone();
+            self.rt.spawn(async move {
+                let resp = tmdb_client.search_movies(&search_text).await;
+                tx.send(resp).unwrap();
+            });
+        }
+
         egui::CentralPanel::default().show(ctx, |ui| self.show(ui));
     }
 }
@@ -56,6 +72,7 @@ impl LichtApp {
             tx,
             rx,
             movie_search_response: None,
+            last_change_time: Instant::now(),
         }
     }
 
@@ -66,18 +83,9 @@ impl LichtApp {
     }
 
     fn search(&mut self, ui: &mut egui::Ui) {
-        ui.horizontal(|ui| {
-            ui.text_edit_singleline(&mut self.search_text);
-            if ui.button("Search").clicked() {
-                let tmdb_client = self.tmdb_client.clone();
-                let search_text = self.search_text.clone();
-                let tx = self.tx.clone();
-                self.rt.spawn(async move {
-                    let resp = tmdb_client.search_movies(&search_text).await;
-                    tx.send(resp).unwrap();
-                });
-            }
-        });
+        if ui.text_edit_singleline(&mut self.search_text).changed() {
+            self.last_change_time = Instant::now();
+        }
     }
 
     fn movie_results(&mut self, ui: &mut egui::Ui) {
@@ -85,23 +93,29 @@ impl LichtApp {
             ScrollArea::vertical().show(ui, |ui| {
                 ui.separator();
                 for result in &resp.results {
-                    ui.horizontal(|ui| {
-                        if let Some(poster_path) = &result.poster_path {
-                            ui.add(Image::new(format!(
-                                "https://image.tmdb.org/t/p/w600_and_h900_bestv2{}",
-                                poster_path
-                            )).fit_to_exact_size(egui::vec2(60.0, 90.0)));
-                        } else {
-                            ui.add(Image::new("https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg")
-                                .fit_to_exact_size(egui::vec2(60.0, 90.0)));
-                        }
-
-                        ui.label(&result.original_title);
-                        ui.label(&format!("({})", result.release_date));
-                    });
+                    self.movie_result(result, ui);
                     ui.separator();
                 }
             });
         }
+    }
+
+    fn movie_result(&self, result: &MovieSearchResult, ui: &mut egui::Ui) {
+        ui.horizontal(|ui| {
+            if let Some(poster_path) = &result.poster_path {
+                ui.add(Image::new(format!(
+                    "https://image.tmdb.org/t/p/w600_and_h900_bestv2{}",
+                    poster_path
+                )).fit_to_exact_size(egui::vec2(90.0, 135.0)));
+            } else {
+                ui.add(Image::new("https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg")
+                    .fit_to_exact_size(egui::vec2(90.0, 135.0)));
+            }
+
+            ui.vertical(|ui| {
+                ui.label(&result.original_title);
+                ui.label(&format!("({})", result.release_date));
+            });
+        });
     }
 }
